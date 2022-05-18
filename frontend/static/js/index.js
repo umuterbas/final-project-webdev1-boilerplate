d76b842a8dec4d30b9f1aa8ade46fdd5cabc639d
Yuto Yamakita <yuvto.0420@gmail.com>
2022-05-18 16:06:44 -0700

    delete function

--- frontend/static/js/index.js
+++ frontend/static/js/index.js
@@ -2,14 +2,5 @@ import {getCountries} from './countries.js'
 
 export const countries = getCountries();
 
-// const display = document.querySelector("#app");
-// const displayAllCountries = () =>{
-//     countries.forEach(element => {
-//         return <div>{element.name}</div>
-//     });
-// }
-
-// displayAllCountries();
 console.log(countries);
 
-// export default displayAllCountries;
