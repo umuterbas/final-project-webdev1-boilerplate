48d1c639d517210a3658f3c1406619c882e48139
Nicolas Meza <nickismeza@gmail.com>
2022-05-20 14:06:06 -0700

    filter added

--- frontend/static/js/views/Countries.js
+++ frontend/static/js/views/Countries.js
@@ -61,9 +61,11 @@ export default class extends AbstractView{
                         <div class="dropdown-menu">
                             <button class="dropbtn">Filter by Region</button>
                                 <div class="dropdown-content">
-                                    <a href="#">Link 1</a>
-                                    <a href="#">Link 2</a>
-                                    <a href="#">Link 3</a>
+                                    <a class="dropdown-item" id="africa">Africa</a>
+                                    <a class="dropdown-item"id="america">America</a>
+                                    <a class="dropdown-item" id="asia" >Asia</a>
+                                    <a  class="dropdown-item" id="europe" >Europe</a>
+                                    <a class="dropdown-item" id="oceania" >Oceania</a>
                                 </div>
                         </div>
                         <svg class="caret-down"  xmlns="http://www.w3.org/2000/svg" viewBox="0 0 320 512"><!--! Font Awesome Pro 6.1.1 by @fontawesome - https://fontawesome.com License - https://fontawesome.com/license (Commercial License) Copyright 2022 Fonticons, Inc. --><path d="M310.6 246.6l-127.1 128C176.4 380.9 168.2 384 160 384s-16.38-3.125-22.63-9.375l-127.1-128C.2244 237.5-2.516 223.7 2.438 211.8S19.07 192 32 192h255.1c12.94 0 24.62 7.781 29.58 19.75S319.8 237.5 310.6 246.6z"/></svg>
