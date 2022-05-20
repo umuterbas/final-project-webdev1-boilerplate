164a0a0077ef1ded416e0362c2984e7fdefbda31
Nicolas Meza <nickismeza@gmail.com>
2022-05-20 08:14:47 -0700

    searchbar added

--- frontend/static/js/index.js
+++ frontend/static/js/index.js
@@ -5,75 +5,79 @@ export const countries = getCountries();
 console.log(countries);
 
 window.addEventListener("DOMContentLoaded", (event) => {
-  console.log("DOM fully loaded and parsed");
-  //Get DOM
-  let slides = document.querySelectorAll(".slider__image");
-//   let navLinks = document.querySelectorAll(".slider__navlink");
-//   const nextButton = document.querySelector("#nav-button--next");
-//   const prevButton = document.querySelector("#nav-button--prev");
+  
+    console.log("DOM fully loaded and parsed");
+    //Get DOM
+    let slides = document.querySelectorAll(".slider__image");
+  //   let navLinks = document.querySelectorAll(".slider__navlink");
+  //   const nextButton = document.querySelector("#nav-button--next");
+  //   const prevButton = document.querySelector("#nav-button--prev");
 
-  let currentSlide = 0;
+    let currentSlide = 0;
 
-  const nextSlide = ()=>{
-    (currentSlide == slides.length - 1)? currentSlide = 0 : currentSlide++;
-    // console.log(currentSlide);
-    // navLinkChange();
-    changeSlide();
-    resetTimer();
-  }
-
-  const prevSlide = () =>{
-    (currentSlide == 0)? currentSlide = slides.length - 1 : currentSlide--;
-    // console.log(currentSlide);
-    // navLinkChange();
-    changeSlide();
-    resetTimer();
-  }
-
-  const changeSlide = ()=>{
-    for (let i = 0; i < slides.length; i++) {
-      slides[i].classList.remove("active");
+    const nextSlide = ()=>{
+      (currentSlide == slides.length - 1)? currentSlide = 0 : currentSlide++;
+      // console.log(currentSlide);
+      // navLinkChange();
+      changeSlide();
+      resetTimer();
     }
-    slides[currentSlide].classList.add("active");
-  }
 
-//   const navLinkChange = ()=>{
-//     for (let i = 0; i < navLinks.length; i++) {
-//       navLinks[i].classList.remove("active");
-//     }
-//     navLinks[currentSlide].classList.add("active");
-//   }
+    const prevSlide = () =>{
+      (currentSlide == 0)? currentSlide = slides.length - 1 : currentSlide--;
+      // console.log(currentSlide);
+      // navLinkChange();
+      changeSlide();
+      resetTimer();
+    }
 
-  //Click Events
-//   nextButton.addEventListener("click", () => {
-//     nextSlide();
-//   });
+    const changeSlide = ()=>{
+      for (let i = 0; i < slides.length; i++) {
+        slides[i].classList.remove("active");
+      }
+      slides[currentSlide].classList.add("active");
+    }
 
-//   prevButton.addEventListener("click", ()=>{
-//     prevSlide();
-//   })
+ 
 
-  const autoChange = ()=>{
-    // console.log("auto");
-    // navLinkChange();
-    nextSlide();
-  }
+    const autoChange = ()=>{
+      // console.log("auto");
+      // navLinkChange();
+      nextSlide();
+    }
 
-  let timer = setInterval(autoChange, 5000);
+    // let timer = setInterval(autoChange, 5000);
 
-  //when either button or indicator(nav links) are clicked, stop the timer and then start again
-  const resetTimer = () =>{
-    clearInterval(timer);
-    timer = setInterval(autoChange, 5000);
-  }
+    //when either button or indicator(nav links) are clicked, stop the timer and then start again
+    const resetTimer = () =>{
+      clearInterval(timer);
+      timer = setInterval(autoChange, 5000);
+    }
 
 
-  //Search
-  const searchBar = document.querySelector("#searchBar");
-  console.log(searchBar);
-  searchBar.addEventListener("keyup", (e)=>{
-    console.log(e.target.value);
-    // let inputText = searchBar
-    // console.log(inputText);
-  })
-});
+    //Search
+    const searchBar = document.querySelector("#searchBar");
+    let countryCard = document.querySelectorAll(".country-card");
+    let inputValue=""
+    let updatedInfo=""
+    searchBar.addEventListener("keyup", function(e){
+      inputValue=e.target.value;
+      updatedInfo=countries.filter((country)=>{
+        return country.name.common.toLowerCase().indexOf(inputValue)!==-1
+      });
+      
+
+    countryCard.forEach(element=>{
+      let nam=document.querySelectorAll("#nam")
+      if(!(updatedInfo===nam.value)){
+        element.style.display="none"
+      }
+    })
+
+  });
+
+  // Displaying searchBar
+  
+
+
+})
--- frontend/static/js/views/Countries.js
+++ frontend/static/js/views/Countries.js
@@ -8,6 +8,7 @@ export default class extends AbstractView{
     }
 
     async getHTML() {
+
         console.log(countries);
         // const countryCard = `
         //      <div class="country-card">
@@ -29,7 +30,7 @@ export default class extends AbstractView{
         //     </div>
         // `;
 
-        
+      
 
         const countryCards = () =>{
             let card ='';
@@ -61,7 +62,7 @@ export default class extends AbstractView{
                           element.flags.png
                         }" alt=""></img>
                             <div class="country-info">
-                                <h3 class="margin-bottom margin-top">${
+                                <h3 class="margin-bottom margin-top" id="nam">${
                                   element.name.common
                                 }</h3>
                                 <div class="flex"><p class="margin-right"><b>Population:</b> </p><p> ${element.population.toLocaleString(
@@ -94,7 +95,7 @@ export default class extends AbstractView{
             <section class="input-container">
                 <div class="input">
                     <div class="search-bar-container center">
-                        <a href="" class="center"><svg class="search-icon" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 512 512"><!--! Font Awesome Pro 6.1.1 by @fontawesome - https://fontawesome.com License - https://fontawesome.com/license (Commercial License) Copyright 2022 Fonticons, Inc. --><path d="M500.3 443.7l-119.7-119.7c27.22-40.41 40.65-90.9 33.46-144.7C401.8 87.79 326.8 13.32 235.2 1.723C99.01-15.51-15.51 99.01 1.724 235.2c11.6 91.64 86.08 166.7 177.6 178.9c53.8 7.189 104.3-6.236 144.7-33.46l119.7 119.7c15.62 15.62 40.95 15.62 56.57 0C515.9 484.7 515.9 459.3 500.3 443.7zM79.1 208c0-70.58 57.42-128 128-128s128 57.42 128 128c0 70.58-57.42 128-128 128S79.1 278.6 79.1 208z"/></svg></a>
+                        <figure class="center" id="searchBtn"><svg class="search-icon" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 512 512"><!--! Font Awesome Pro 6.1.1 by @fontawesome - https://fontawesome.com License - https://fontawesome.com/license (Commercial License) Copyright 2022 Fonticons, Inc. --><path d="M500.3 443.7l-119.7-119.7c27.22-40.41 40.65-90.9 33.46-144.7C401.8 87.79 326.8 13.32 235.2 1.723C99.01-15.51-15.51 99.01 1.724 235.2c11.6 91.64 86.08 166.7 177.6 178.9c53.8 7.189 104.3-6.236 144.7-33.46l119.7 119.7c15.62 15.62 40.95 15.62 56.57 0C515.9 484.7 515.9 459.3 500.3 443.7zM79.1 208c0-70.58 57.42-128 128-128s128 57.42 128 128c0 70.58-57.42 128-128 128S79.1 278.6 79.1 208z"/></svg></figure>
                         <input name="word" id="searchBar" class="search-bar" type="text" placeholder="Search for a country..">
                     </div>
                     <div class="dropdown">
@@ -115,5 +116,8 @@ export default class extends AbstractView{
             ${countryCards()}
             </main>`;
     }
+
+    
+
 }
 
